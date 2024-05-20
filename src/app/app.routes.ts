9cfc67dc3748d4fbbb4fca7052f2ffc464cd2ed6
sahal <sahalkdr@gmail.com>
2024-05-20 08:35:52 +0400

    material added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {LoginComponent} from "./layouts/login/login.component";
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent }
+];
