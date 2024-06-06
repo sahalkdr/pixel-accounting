895a6f9676391e5d97de3edda037d5a457534f40
sreevidyamadambath <sreevidyam07@gmail.com>
2024-06-06 09:26:49 +0530

    commit

--- src/app/layouts/reports/sale/sale.component.ts
+++ src/app/layouts/reports/sale/sale.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-
 import { MatIconModule } from '@angular/material/icon';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 interface SaleDetails {
@@ -25,17 +25,18 @@ interface SaleDetails {
 
 @Component({
   selector: 'app-sale',
-  standalone:true,
-  imports:[CommonModule,MatIconModule],
+  standalone: true,
+  imports: [CommonModule, MatIconModule,NgxPaginationModule],
   templateUrl: './sale.component.html',
   styleUrls: ['./sale.component.scss']
 })
 export class SaleComponent implements OnInit {
   saledetails: SaleDetails = { company_info: { name: '', place: '', phone: '' }, bills: [] };
-  billId: number = 0;
   totalPaidAmount: number = 0;
+  p:number=1;
+  itemsPerPage:number=13;
 
-  constructor(private http: HttpClient,private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.fetchSaleDetails();
@@ -45,42 +46,36 @@ export class SaleComponent implements OnInit {
     this.router.navigate(['/sale-details']);
   }
 
-
-  // viewBillDetails(): void {
-  //   this.router.navigate(['/bill-details'], { queryParams: { bill_id: this.billId } });
-  // }
-
   viewBillDetails(billId: string): void {
     this.router.navigate(['/bill-details'], { queryParams: { bill_id: billId } });
-}
-calculateTotalPaidAmount() {
-  this.totalPaidAmount = this.saledetails.bills.reduce((total, bill) => total + parseFloat(bill.total_amount), 0);
-}
-
-filterByDateRange() {
-  const fromDate = (<HTMLInputElement>document.getElementById('from-date')).value;
-  const toDate = (<HTMLInputElement>document.getElementById('to-date')).value;
-  const partyName = (<HTMLInputElement>document.getElementById('party-name')).value;
-
-  let url = `http://localhost/restaurant/get_all_bill_details.php?from_date=${fromDate}&to_date=${toDate}`;
-  
-  if (partyName.trim() !== '') {
-      url += `&party_name=${encodeURIComponent(partyName)}`;
   }
-  
-  this.http.get<SaleDetails>(url).subscribe(
+
+  calculateTotalPaidAmount() {
+    this.totalPaidAmount = this.saledetails.bills.reduce((total, bill) => total + parseFloat(bill.total_amount), 0);
+  }
+
+  filterByDateRange() {
+    const fromDate = (document.getElementById('from-date') as HTMLInputElement).value;
+    const toDate = (document.getElementById('to-date') as HTMLInputElement).value;
+    const partyName = (document.getElementById('party-name') as HTMLInputElement).value;
+
+    let url = `http://localhost/restaurant/get_all_bill_details.php?from_date=${fromDate}&to_date=${toDate}`;
+
+    if (partyName.trim() !== '') {
+      url += `&party_name=${encodeURIComponent(partyName)}`;
+    }
+
+    this.http.get<SaleDetails>(url).subscribe(
       (resp: SaleDetails) => {
-          console.log(resp);
-          this.saledetails = resp;
-          this.calculateTotalPaidAmount();
+        console.log(resp);
+        this.saledetails = resp;
+        this.calculateTotalPaidAmount();
       },
       error => {
-          console.error('Error:', error);
+        console.error('Error:', error);
       }
-  );
-}
-
-
+    );
+  }
 
   fetchSaleDetails() {
     this.http.get<SaleDetails>('http://localhost/restaurant/get_all_bill_details.php').subscribe(
